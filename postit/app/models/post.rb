86da7b2c56cf750771003cf5431c75609204ee6d
marvinick <marviluw@gmail.com>
2013-09-09 15:43:02 -0500

    partial

--- postit/app/models/post.rb
+++ postit/app/models/post.rb
@@ -1,6 +1,4 @@
 class Post < ActiveRecord::Base 
   belongs_to :user
-  has_many :comments 
-  has_many :post_categories
-  has_many :categories, through: :post_categories
+  has_many :comments
 end 
--- postit/db/schema.rb
+++ postit/db/schema.rb
@@ -11,24 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908174714) do
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20130909202952) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "post_categories", force: true do |t|
-    t.integer  "category_id"
     t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
