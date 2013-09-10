1d94b4c46c1b84330dc3a85d2cdf9721a37fde74
marvinick <marviluw@gmail.com>
2013-09-10 16:08:52 -0500

    almost there

--- postit/app/models/category.rb
+++ postit/app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base 
-  has_many :post_categories 
-  has_many :posts, through: :post_categories
-end 
+	has_many :post_categories 
+	has_many :posts, through: :post_categories 
+end  
