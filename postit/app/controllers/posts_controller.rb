fd2a2310e37d4ec05ad0ce2607a8eac7780225c8
marvinick <marviluw@gmail.com>
2013-09-11 08:36:51 -0500

    form

--- postit/app/controllers/posts_controller.rb
+++ postit/app/controllers/posts_controller.rb
@@ -1,5 +1,34 @@
 class PostsController < ApplicationController
+  
   def index
-
+    @posts = Post.all 
   end
+
+  def show 
+    @post = Post.find(params[:id])
+  end 
+
+  def new 
+    @post = Post.new 
+  end 
+
+  def create 
+    binding.pry 
+    post = Post.new(post_params)
+  end 
+
+  def edit 
+    @post = Post.find(params[:id])
+  end 
+
+  def update
+  
+  end 
+
+  private 
+
+  def post_params 
+  	params.require(:post).permit!
+  end 
+
 end
